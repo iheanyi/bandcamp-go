41eb889502186c899e35651ec8c6e7d8e148c4d5
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2016-08-19 00:58:29 -0400

    Define the test cases for things I'll probably want to test in my code.

--- bandcamp_test.go
+++ bandcamp_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+  "testing"
+)
+
+func TestExecuteCode(t *testing.T) {
+}
+
+func TestFetchPage(t *testing.T) {
+}
+
+func TestDownloadTrack(t *testing.T) {
+}
+
+func TestDownloadAlbum(t *testing.T) {
+}
+
+func TestIsAlbum(t *testing.T) {
+}
+
+func TestSetMetadata(t *testing.T) {
+}
